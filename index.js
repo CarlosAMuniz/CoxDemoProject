d0ce1d83c0b090a89c2495715d7a3c4dba1286a6
CarlosAMuniz <carlosmu98@yahoo.com>
2016-12-18 09:07:06 -0500

    Remove dead code
    The original code include functions used by the tutorial used to prepared.  They should not be part of the final solution

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var requestHandlers = require("./requestHandlers");
 
 var handle = {};
 handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
 handle["/reserveTimeSlot"] = requestHandlers.reserveTimeSlot;
 handle["/reservingTimeSlot"] = requestHandlers.reservingTimeSlot;
 server.start(router.route, handle);
